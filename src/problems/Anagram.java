c36f5cefb7e1a97a3b630df17eadac0d0b9e0ac4
uzmajam <bluebottle786@hotmail.com>
2018-12-13 15:17:45 -0500

    new commit

--- src/problems/Anagram.java
+++ src/problems/Anagram.java
@@ -8,5 +8,7 @@ public class Anagram {
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
+        String name = "MARY";
     }
 }
